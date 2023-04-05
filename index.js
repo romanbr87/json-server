a050fb3e60595844c16bc9e8b8703035cf12b270
Roman Braverman <75772227+romanbr87@users.noreply.github.com>
2023-04-06 00:31:45 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const path = require('path');
 var logger = require('morgan');
-const cookieParser = require('cookie-parser');
+//const cookieParser = require('cookie-parser');
 const express = require('express');
-//const register = require('@react-ssr/express/register');
 const cors = require('cors')
 const bodyParser = require('body-parser');
-//const ReactDOMServer = require('react-dom/server');
 require('dotenv').config();
 //const routes = require('./server/routers/routes.js');
 const nocache = require("nocache");
@@ -16,15 +14,17 @@ let rawdata = fs.readFileSync('db.json');
 let db = JSON.parse(rawdata, null, 2);
 
 /*app.use(function(req, res, next) { 
-    if (req.method == 'GET') console.log ('ff')
+    /*console.log (req.body);
+    if (req.method == 'POST')
+        if (req.body.key !== 'romanbr87') res.json (null)
     next();
 });*/
   
 app.get('/favicon.ico', function(req, res, next) { 
-  //res.sendStatus(204); 
-  next();
+    res.sendStatus(204); 
+    //next();
 });
-
+  
 
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: false }));
@@ -37,20 +37,35 @@ app.use(logger('dev'));
 //app.use(express.static(path.join(__dirname, 'public')));
 
 //app.use('/', routes);
-app.get('/', function(req, res, next) { 
-    res.json(db); 
+/*app.get('/', function(req, res, next) {
+    res.json ({ status: 40412, message: "HELLO"})
+})*/
+
+
+app.post('/', function(req, res, next) { 
+    res.json(db.json); 
 });
 
-app.get('/:id', function(req, res, next) { 
+app.post('/:id', function(req, res, next) { 
     res.json(db.job[req.params.id]); 
 });
 
-app.get('/:id1/:id2', function(req, res, next) { 
+app.post('/:id1/:id2', function(req, res, next) { 
     res.json(db.job[req.params.id1].links[req.params.id2]); 
 });
 
-app.get('/:id1/:id2/:id3', function(req, res, next) { 
+app.post('/:id1/:id2/:id3', function(req, res, next) { 
     res.json(db.job[req.params.id1].links[req.params.id2].links[req.params.id3]); 
 });
+
+app.get('/*', function(req, res, next) {
+    res.json ({ status: 404, message: "הדף לא קיים"})
+})
+  
+app.post('/*', function(req, res, next) {
+    res.status (404).json ({ status: 404, message: "הדף לא קיים"});
+})
+  
+  
 
 module.exports = app;
